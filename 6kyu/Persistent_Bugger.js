959f727130e1bdf70176c1b2014ed057b986de26
dima <zhuikovda.gmail.com>
2022-11-30 22:34:20 +0300

    Persistent_Bugger

---
+++ 6kyu/Persistent_Bugger.js
@@ -0,0 +1,12 @@
+function persistence(num) {
+    //code me
+    let res = 0
+    let str = String(num);    
+    for (let i = 0; str.length > 1; i++) {
+        str = String([...str].reduce((acc, curr) => acc * curr));
+        res++;
+    }
+    console.log(res)
+ }
+
+ persistence(999);
