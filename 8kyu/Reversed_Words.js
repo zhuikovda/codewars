0d6f70292736cf33667920ffd448440ef912a728
dima <zhuikovda@gmail.com>
2023-01-12 21:41:52 +0300

    Reversed_Words

---
+++ 8kyu/Reversed_Words.js
@@ -0,0 +1,9 @@
+function reverseWords(str){
+    // let a = 
+    // let s = 
+console.log(str.split(" ").reverse().join(" "));
+
+    // return str; // reverse those words
+  }
+
+  reverseWords("The greatest victory is that which requires no battle") 
