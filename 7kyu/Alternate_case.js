678f1ffa5abbcd22217f56c5890acb386b1c75d4
dima <zhuikovda@gmail.com>
2023-06-09 23:00:28 +0300

    Alternate_case

---
+++ 7kyu/Alternate_case.js
@@ -0,0 +1,16 @@
+"use strict";
+const alternateCase = (s) => {
+  //   const sSplit = s.split("");
+  //   const convert = sSplit.map((t) =>
+  //     t === t.toUpperCase() ? t.toLowerCase() : t.toUpperCase()
+  //   );
+  //   console.log(convert.join(""));
+  console.log(
+    s
+      .split("")
+      .map((t) => (t === t.toUpperCase() ? t.toLowerCase() : t.toUpperCase()))
+      .join("")
+  );
+};
+
+alternateCase("ABC");
