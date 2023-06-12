9df7a9c1b564487dbc02f6ae48fc88e50e787edd
dima <zhuikovda@gmail.com>
2023-06-12 12:35:50 +0300

    finish Basic_JS_Building_a_calculator

--- 7kyu/Basic_JS_Building_a_calculator.js
+++ 7kyu/Basic_JS_Building_a_calculator.js
@@ -1,3 +1,25 @@
-const caldAdd = ()=> {
-    
-}
+var Calculator = {
+  add: function add(a, b) {
+    console.log(a + b);
+  },
+  subtract: function subtract(a, b) {
+    console.log(a - b);
+  },
+  multiply: function subtract(a, b) {
+    console.log(a * b);
+  },
+  divide: function subtract(a, b) {
+    console.log(b !== 0 ? a / b : false);
+  },
+};
+
+
+// не мое решение
+// var Calculator = {
+//   add:     (a, b) => a + b,
+//   subtract:(a, b) => a - b,
+//   multiply:(a, b) => a * b,
+//   divide:  (a, b) => b ? a / b : false
+// };
+
+Calculator.divide(6, 0);
